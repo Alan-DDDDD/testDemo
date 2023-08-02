8edfbea237bb5f8d4093c6d382e3997b59cc1191
Alan-DDDDD <115976002+Alan-DDDDD@users.noreply.github.com>
2023-08-02 15:05:32 +0800

    Add files via upload

---
+++ account.js
@@ -0,0 +1,19 @@
+getAccountData();
+async function getAccountData(){
+    var response = await fetch("https://localhost:7285/api/Account");
+    var source = await response.json();
+    console.log(source);
+    if(source.status == 0){
+        var data = source.data;
+        var table = $(`#account tbody`);
+        $.each(data,function(index,value){
+            table.append(`<tr>
+                <td>${value.userid}</td>
+                <td>${value.bankname}</td>
+                <td>${value.bankcode}</td>
+                <td>${value.bankaccount}</td>
+                <td>${value.amount}</td>
+            </tr>`)
+        })
+    }
+}
